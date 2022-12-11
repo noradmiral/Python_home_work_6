ba43bc995c0fecf33fbe5ff180bf22d2f4a00009
Сергей Нестеров <93441101+noradmiral@users.noreply.github.com>
2022-12-11 23:36:20 +0700

    Python_home_work_

---
+++ task1.py
@@ -0,0 +1,18 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# Пример:
+# - 6,782 -> 23
+# - 0,56 -> 11
+
+# def sum (number):
+#     sum = 0
+#     for i in number:
+#         if i != '.':
+#             sum = sum + int(i)
+#     return sum
+
+
+# number = input('Введите вещественное число: ')
+
+# print (sum(number))
+
+print(sum(list(map(lambda x: 0 if x == '.' else int(x), input('введите вещественное число: ')))))
